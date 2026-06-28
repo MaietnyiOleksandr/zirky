975ad06a820d66979b9120efd0c241b1b1d16019
MaietnyiOleksandr <oleksandr.maietnyi@users.noreply.github.com>
2026-06-28 09:54:34 +0300

    оновлення версії

--- appearance.js
+++ appearance.js
@@ -12,7 +12,7 @@
 //       3. Додай CSS vars у style.css (опційно)
 // ════════════════════════════════════════════════════
 
-export const VERSION = 'v4.20260627.2328';
+export const VERSION = 'v4.20260628.0954';
 
 import { state } from './state.js';
 import { saveAppearance, saveParentAppearance, saveRecords, saveBorder, saveChildMeta } from './firebase.js';
